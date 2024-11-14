ae45f85df68774d605b75a59b89220ca829d4a6e
bedv-jw <0523wns@naver.com>
2024-11-14 14:07:01 +0900

    update Filder

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -19,7 +19,7 @@ const CreatePost = () => {
     if (form.prompt) {
       try {
         setGeneratingImg(true);
-        const response = await fetch('http://localhost:8080/api/v1/stable-diffusion', {
+        const response = await fetch('https://stablediffusion-gen-ai-image.onrender.com/api/v1/stable-diffusion', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
@@ -48,7 +48,7 @@ const CreatePost = () => {
     if(form.prompt && form.photo){
       setLoading(true)
       try {
-        const response = await fetch('http://localhost:8080/api/v1/post', {
+        const response = await fetch('https://stablediffusion-gen-ai-image.onrender.com/api/v1/post', {
           method:'POST',
           headers: {'Content-Type': 'application/json'
           },
